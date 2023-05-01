635bc80cb0958e3b0088947799ad64b7fd0d94dc
Arsh <arshahmed235@gmail.com>
2023-05-01 20:21:55 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const navCloseIcon = document.querySelector('[data-close-icon]')
+const navOpenIcon = document.querySelector('[data-open-icon]')
+const questionEl = document.querySelectorAll('.question')
+const questionPEl = document.querySelectorAll('.question p')
+const navBarEl = document.querySelector('[data-navbar]')
+const overlayEl = document.querySelector('[data-overlay]')
+
+const addEventOnElements = (element, eventType, callback) => {
+    element.addEventListener(eventType, callback)
+}
+
+addEventOnElements(navOpenIcon, 'click', () => {
+    navBarEl.classList.add('active')
+    overlayEl.classList.add('active')
+})
+
+addEventOnElements(navCloseIcon, 'click', () => {
+    navBarEl.classList.remove('active')
+    overlayEl.classList.remove('active')
+})
+
+questionEl.forEach(element => {
+    element.addEventListener('click', () => {
+        let p = element.querySelector('p')
+        p.classList.toggle('active')
+    })
+});
