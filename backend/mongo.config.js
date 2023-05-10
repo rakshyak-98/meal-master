d4c86632e0e202fcefb14ef0ead2544bac79c9ce
rak-98-dev <rakshak1998@gmail.com>
2023-05-11 00:27:05 +0530

    changed env name.

--- backend/mongo.config.js
+++ backend/mongo.config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 exports.connect = () => {
   try {
     mongoose.connect(
-      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.CLUSTER_STRING}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_STRING}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
     );
   } catch (error) {
     return error.message;
