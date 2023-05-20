7b2846d75c6ece97f75cf58f0ff52d09b553717a
rak-98-dev <rakshak1998@gmail.com>
2023-05-20 13:14:14 +0530

    Modification: backend user sign up flow, and helper packages.

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const {connect} = require('./mongo.config')
-const {user, profile} = require('./models/user')
-
-user.create({username:'rakshyak', password: 'rakshyak1998', role: 'expert'})
-
-// mongodb connection.
-connect()
