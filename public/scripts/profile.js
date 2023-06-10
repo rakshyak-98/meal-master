2fb694501216c2485535be5b0acc76580c27d1e4
rakshyak-98 <rakshak1998@gmail.com>
2023-06-10 12:38:40 +0530

    new: sending data of new query to backend

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -4,24 +4,25 @@ newQuery = optionList.querySelector("#newQuery");
 savedPlans = optionList.querySelector("#savedPlans");
 profileView = document.querySelector("#profile-view");
 symptomBlock = document.querySelector(".symptoms");
-
-symptomSelectionList = profileView.querySelector(".symptom-selection");
-
-symptomSelectionList.onchange = (e) => {
-  if (e.target.value == "none") {
-    profileView.querySelector(".enter-info").style.display = "flex";
-    profileView.querySelector(".symptom-list").style.display = "none";
-  } else {
-    profileView.querySelector(".enter-info").style.display = "none";
-    profileView.querySelector(".symptom-list").style.display = "flex";
-  }
-};
+postButton = document.querySelector("#post-symptom");
 
 window.onhashchange = () => {
-  profileView.dataset.view = location.hash.replace("#", "")
+  profileView.dataset.view = location.hash.replace("#", "");
 };
 
 window.onload = () => {
-  profileView.querySelector(".enter-info").style.display = "none";
   token = localStorage.getItem("token");
+};
+
+postButton.onclick = (e) => {
+  formdata = new FormData(document.forms.newQuery);
+  postTitle = formdata.get("title");
+  postDescription = formdata.get("description");
+
+  if (postTitle && postDescription) {
+    fetch("http://localhost:3001/post", {
+      method: "post",
+      body: JSON.stringify({ title: postTitle, description: postDescription }),
+    });
+  }
 };
