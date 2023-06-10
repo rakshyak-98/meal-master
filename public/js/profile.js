0244646695fd0e3e31c5c3d6fe8e424f1e353731
rakshyak-98 <rakshak1998@gmail.com>
2023-06-10 10:53:35 +0530

    new: profile ui new query

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,27 +1,35 @@
-optionList = document.querySelector(".profile__bar ul")
-myQuery = optionList.querySelector('#myQuery')
-newQuery = optionList.querySelector('#newQuery')
-savedPlans = optionList.querySelector('#savedPlans')
+optionList = document.querySelector(".profile__bar ul");
+myQuery = optionList.querySelector("#myQuery");
+newQuery = optionList.querySelector("#newQuery");
+savedPlans = optionList.querySelector("#savedPlans");
 
-profileView = document.querySelector('#profile-view')
+profileView = document.querySelector("#profile-view");
 
-symptomSelection = profileView.querySelector("#symptom-selection")
+symptomSelection = profileView.querySelector("#symptom-selection");
 
 symptomSelection.onchange = (e) => {
-    if(e.target.value == "none") profileView.querySelector('.enter-info').style.display = "block"
-    else profileView.querySelector('.enter-info').style.display = "none"
-}
+  if (e.target.value == "none") {
+    profileView.querySelector(".enter-info").style.display = "block";
+    profileView.querySelector(".symptom-list").style.display = "none";
+} else {
+    profileView.querySelector(".enter-info").style.display = "none";    
+    profileView.querySelector(".symptom-list").style.display = "block";
+  }
+};
 
+window.onhashchange = () => {
+  location.hash != "#my-query"
+    ? (profileView.querySelector(".page-navigation").style.display = "none")
+    : (profileView.querySelector(".page-navigation").style.display = "block");
+};
 
 window.onload = () => {
-    profileView.querySelector('.enter-info').style.display = 'none'
-}
-
-
-
+  profileView.querySelector(".enter-info").style.display = "none";
+};
 
 // html templates
-{/* <div data-query="none" class="enter-info">
+{
+  /* <div data-query="none" class="enter-info">
     <input type="text" placeholder="Enter disesess name" />
     <div>
         <p>Recomended by doctor</p>
@@ -33,4 +41,5 @@ window.onload = () => {
         >
     </div>
     <input type="text" placeholder="Enter doctor name" />
-</div> */}
+</div> */
+}
