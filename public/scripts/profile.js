5a8f67c4cca1f13e1786b4b7ceac499c89bc5a16
rakshyak-98 <rakshak1998@gmail.com>
2023-06-16 15:31:51 +0530

    modify: remove backed code from UI branch

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,10 +1,11 @@
-optionList = document.querySelector(".profile__bar ul");
-myQuery = optionList.querySelector("#myQuery");
-newQuery = optionList.querySelector("#newQuery");
-savedPlans = optionList.querySelector("#savedPlans");
-profileView = document.querySelector("#profile-view");
-symptomBlock = document.querySelector(".symptoms");
-postButton = document.querySelector("#post-symptom");
+const optionList = document.querySelector(".profile__bar ul");
+const myQuery = optionList.querySelector("#myQuery");
+const newQuery = optionList.querySelector("#newQuery");
+const savedPlans = optionList.querySelector("#savedPlans");
+const profileView = document.querySelector("#profile-view");
+const symptomBlock = document.querySelector(".symptoms");
+const postButton = document.querySelector("#post-symptom");
+const profileInfo = document.querySelector(".profile__info");
 
 window.onhashchange = () => {
   profileView.dataset.view = location.hash.replace("#", "");
@@ -12,6 +13,23 @@ window.onhashchange = () => {
 
 window.onload = () => {
   token = localStorage.getItem("token");
+  authString = `Bearer ${token}`;
+  fetch("http://localhost:3001/user", {
+    method: "GET",
+    headers: { Authorization: authString },
+  })
+    .then((res) =>
+      res.json().then((data) => {
+        const { user: {username}} = data;
+        const usernameEl = document.createElement('h2')
+        const roleEl = document.createElement('p')
+        usernameEl.textContent = username
+        roleEl.textContent = 'Expert'
+        profileInfo.appendChild(usernameEl)
+        profileInfo.appendChild(roleEl)
+      })
+    )
+    .catch((error) => console.error(error.message));
 };
 
 postButton.onclick = (e) => {
@@ -23,6 +41,6 @@ postButton.onclick = (e) => {
     fetch("http://localhost:3001/post", {
       method: "post",
       body: JSON.stringify({ title: postTitle, description: postDescription }),
-    });
+    }).catch((error) => console.error(error));
   }
 };
