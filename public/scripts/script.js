9fd74db68749089150e02e4751193212641cc416
rakshyak-98 <rakshyak1998@gmail.com>
2023-09-16 22:01:56 +0530

    refactor file folder structure

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,27 +1,40 @@
-// const navCloseIcon = document.querySelector('[data-close-icon]')
-// const navOpenIcon = document.querySelector('[data-open-icon]')
-// const questionEl = document.querySelectorAll('.question')
-// const questionPEl = document.querySelectorAll('.question p')
-// const navBarEl = document.querySelector('[data-navbar]')
-// const overlayEl = document.querySelector('[data-overlay]')
-
-// const addEventOnElements = (element, eventType, callback) => {
-//     element.addEventListener(eventType, callback)
-// }
-
-// addEventOnElements(navOpenIcon, 'click', () => {
-//     navBarEl.classList.add('active')
-//     overlayEl.classList.add('active')
-// })
-
-// addEventOnElements(navCloseIcon, 'click', () => {
-//     navBarEl.classList.remove('active')
-//     overlayEl.classList.remove('active')
-// })
-
-// questionEl.forEach(element => {
-//     element.addEventListener('click', () => {
-//         let p = element.querySelector('p')
-//         p.classList.toggle('active')
-//     })
-// });
+const profileViewContainer = document.getElementById("profile-view");
+profileViewContainer.appendChild(createFeed());
+function createFeed() {
+  const feed = document.createElement("div");
+  feed.setAttribute("class", "feed");
+  const span = document.createElement("span");
+  span.setAttribute("class", "icon");
+  span.setAttribute("id", "brain");
+  const icon = document.createElement("i");
+  icon.setAttribute("class", "fa-solid fa-brain");
+  span.appendChild(icon);
+  feed.appendChild(span);
+  const heading = document.createElement("h2");
+  heading.textContent = "Hypertension medication";
+  heading.setAttribute("class", "mb--1");
+  const symptom = document.createElement("div");
+  symptom.setAttribute("class", "symptom");
+  symptom.appendChild(heading);
+  feed.appendChild(symptom);
+  const para = document.createElement("p");
+  para.setAttribute("class", "mb--2");
+  para.textContent = "this is description";
+  symptom.appendChild(para);
+  const footer = document.createElement("din");
+  footer.setAttribute("class", "footer");
+  const views = document.createElement("span");
+  views.textContent = "16 views";
+  footer.appendChild(views);
+  const time = document.createElement("span");
+  time.textContent = "6:00pm";
+  footer.appendChild(time);
+  symptom.appendChild(footer);
+  return feed;
+}
+const loginBtn = document.getElementById("form");
+loginBtn.addEventListener("submit", login);
+function login(e) {
+  e.preventDefault();
+  location.replace("/profile");
+}
